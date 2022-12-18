84d6d0b7afa2aa61c85809b75cd0b9791def6ff8
raihan512 <raihangazi1024@Gmail.com>
2022-12-18 18:42:16 +0600

    project routes setup

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderTop from './HeaderTop/HeaderTop';
+import Navbar from './Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <HeaderTop></HeaderTop>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Pages/Shared/Header/HeaderTop/HeaderTop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderTop = () => {
+    return (
+        <div>
+            <h3>Headertop</h3>
+        </div>
+    );
+};
+
+export default HeaderTop;
