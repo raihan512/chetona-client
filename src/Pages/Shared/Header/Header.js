32e36022e033c5a849fc1daf3a2ced9ba5cf7090
raihan512 <raihangazi1024@Gmail.com>
2022-12-18 19:57:50 +0600

    headertop added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,9 +4,11 @@ import Navbar from './Navbar/Navbar';
 
 const Header = () => {
     return (
-        <div>
-            <HeaderTop></HeaderTop>
-            <Navbar></Navbar>
+        <div className='max-width'>
+            <div className='mx-1.5 md:mx-2.5'>
+                <HeaderTop></HeaderTop>
+                <Navbar></Navbar>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'mont': ['Montserrat', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
